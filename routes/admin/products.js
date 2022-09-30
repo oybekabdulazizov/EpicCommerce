7b1ad1335722335ddab4994c8dcb2e22c7973ba1
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-09-30 18:40:09 +0200

    list the products in a separate view

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,12 +5,16 @@ const multer = require('multer');
 const { handleErrors } = require('./middleware');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new')
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
+router.get('/admin/products', async (req, res) => {
+    const products = await productsRepo.getAll();
+    
+    res.send(productsIndexTemplate({ products }));
 });
 
 router.get('/admin/products/new', (req, res) => {
@@ -23,7 +27,8 @@ router.post('/admin/products/new',
     handleErrors(productsNewTemplate),
     async (req, res) => {
 
-        const image = req.file.buffer.toString('base64');
+        console.log(req.file);
+        const image = req.file.buffer.toString('base64') ? buffer.toString('base64') : "";
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });
 
