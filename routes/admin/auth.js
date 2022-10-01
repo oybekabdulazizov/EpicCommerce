828c3e912aa9e23864d95285eda004d4a0e6e456
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-10-01 10:57:46 +0200

    redirect on actions and add authentication to products page

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -24,7 +24,7 @@ router.post('/signup',
     const user = await usersRepo.create({ email, password });
     
     req.session.userId = user.id;
-    res.send('Account created!');
+    res.redirect('/admin/products');
 });
 
 
@@ -49,7 +49,7 @@ router.post('/signin',
     
     req.session.userId = user.id;
 
-    res.send('You are signed in.')
+    res.redirect('/admin/products');
 });
 
 module.exports = router;
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
-const { handleErrors } = require('./middleware');
+const { handleErrors, requireAuth } = require('./middleware');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new')
 const productsIndexTemplate = require('../../views/admin/products/index');
@@ -11,17 +10,17 @@ const { requireTitle, requirePrice } = require('./validators');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
     const products = await productsRepo.getAll();
-    
     res.send(productsIndexTemplate({ products }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
 router.post('/admin/products/new',
+    requireAuth,
     upload.single('image'),
     [requireTitle, requirePrice],
     handleErrors(productsNewTemplate),
@@ -38,6 +37,6 @@ router.post('/admin/products/new',
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });
 
-        res.send('Submitted.');
+        res.redirect('/admin/products');
 })
 module.exports = router;
